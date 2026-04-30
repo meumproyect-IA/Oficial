77022004cf4c1eea111808c5a1ec702464e3c9f2
Germán Ruz <meumproyect@gmail.com>
2026-04-30 14:25:56 -0600

    parece Lumina como CoAutora

--- js/index.js
+++ js/index.js
@@ -4,6 +4,6 @@ function actualizarFecha() {
     const fecha = new Date();
     const opciones = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     let texto = fecha.toLocaleDateString('es-ES', opciones);
-    contenedor.innerHTML = `Actualizado el: ${texto.charAt(0).toUpperCase() + texto.slice(1)}`;
+    contenedor.textContent = `Actualizado el: ${texto.charAt(0).toUpperCase() + texto.slice(1)}`;
 }
 document.addEventListener('DOMContentLoaded', actualizarFecha);
